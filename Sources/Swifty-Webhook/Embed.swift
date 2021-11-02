977c4032e5bdf640b99e655d383ad73a815da950
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2021-11-02 09:14:50 -0400

    Embeds should be finished

--- Sources/Swifty-Webhook/Embed.swift
+++ Sources/Swifty-Webhook/Embed.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  File
+//  Embed.swift
+//  Swifty Webhook
 //
 //  Created by Noah Pistilli on 2021-11-01.
 //
@@ -52,6 +52,26 @@ public struct Embed: Encodable {
       self.type = "rich"
       self.video = nil
     }
+    
+    
+    /**
+     Adds a field to the embed
+     
+     - parameter name: Name to give field
+     - parameter value: Text that will be displayed underneath name
+     - parameter inline: Whether or not to keep this field inline with others
+    */
+    public mutating func addField(
+      _ name: String,
+      value: String,
+      isInline: Bool = false
+    ) {
+      if self.fields == nil {
+        self.fields = [Field]()
+      }
+      
+      self.fields?.append(Field(name: name, value: value, isInline: isInline))
+    }
 }
 
 extension Embed {
@@ -59,42 +79,89 @@ extension Embed {
         public var iconUrl: String?
         public var name: String
         public var url: String?
+        
+        public init(iconUrl: String? = nil, name: String, url: String? = nil) {
+          self.iconUrl = iconUrl
+          self.name = name
+          self.url = url
+        }
     }
     
     public struct Field: Encodable {
-        public var isInline: Bool
         public var name: String
         public var value: String
+        public var isInline: Bool
+        
+        public init(name: String = "", value: String = "", isInline: Bool = true) {
+            self.name = name
+            self.value = value
+            self.isInline = isInline
+        }
     }
     
     public struct Footer: Encodable {
-      public var iconUrl: String?
-      public var proxyIconUrl: String?
-      public var text: String
+        public var iconUrl: String?
+        public var proxyIconUrl: String?
+        public var text: String
+        
+        public init(
+          text: String,
+          iconUrl: String? = nil,
+          proxyIconUrl: String? = nil
+        ) {
+          self.text = text
+          self.iconUrl = iconUrl
+          self.proxyIconUrl = proxyIconUrl
+        }
     }
     
     public struct Image: Encodable {
-      public var height: Int
-      public var proxyUrl: String
-      public var url: String
-      public var width: Int
+        public var height: Int
+        public var proxyUrl: String
+        public var url: String
+        public var width: Int
+        
+        public init(height: Int, proxyUrl: String, url: String, width: Int) {
+          self.height = height
+          self.proxyUrl = proxyUrl
+          self.url = url
+          self.width = width
+        }
     }
     
     public struct Provider: Encodable {
-      public var name: String
-      public var url: String?
+        public var name: String
+        public var url: String?
+        
+        public init(name: String, url: String? = nil) {
+          self.name = name
+          self.url = url
+        }
     }
     
     public struct Thumbnail: Encodable {
-      public var height: Int
-      public var proxyUrl: String
-      public var url: String
-      public var width: Int
+        public var height: Int
+        public var proxyUrl: String
+        public var url: String
+        public var width: Int
+        
+        public init(height: Int, proxyUrl: String, url: String, width: Int) {
+          self.height = height
+          self.proxyUrl = proxyUrl
+          self.url = url
+          self.width = width
+        }
     }
     
     public struct Video: Encodable {
-      public var height: Int
-      public var url: String
-      public var width: Int
+        public var height: Int
+        public var url: String
+        public var width: Int
+        
+        public init(height: Int, url: String, width: Int) {
+            self.height = height
+            self.url = url
+            self.width = width
+        }
     }
 }
