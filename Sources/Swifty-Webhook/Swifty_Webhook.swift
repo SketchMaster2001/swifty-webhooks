b787986447fbb439ad7061932685f3e00edb12c0
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2021-11-02 08:16:19 -0400

    It works now

--- Sources/Swifty-Webhook/Swifty_Webhook.swift
+++ Sources/Swifty-Webhook/Swifty_Webhook.swift
@@ -1,10 +1,17 @@
 import Foundation
 
 open class Webhook {
-    // MARK: Initializer
-    
     /// The webhook's URL that we will post to
     let url: String
+    
+    /// Global URLSession stolen from Sword
+    let session = URLSession(
+      configuration: .default,
+      delegate: nil,
+      delegateQueue: OperationQueue()
+    )
+    
+    // MARK: Initializer
     
     /**
      Initializes the Webhook class
@@ -13,26 +20,37 @@ open class Webhook {
     public init(url: String) {
         self.url = url
     }
+        
+    // MARK: Functions
     
-    public func send(_ webhook: WebhookBody) {
+    /**
+     Sends a POST request to the webhook's URL
+     - parameter message: The message you will be sending
+    */
+    public func send(_ message: WebhookBody) {
         do {
             let encoder = JSONEncoder()
             encoder.keyEncodingStrategy = .convertToSnakeCase
             
-            let data = try encoder.encode(webhook)
+            let data = try encoder.encode(message)
             let url = URL(string: self.url)
             
             var request = URLRequest(url: url!)
             request.httpMethod = "POST"
             request.httpBody = data
             
-            let task = URLSession.shared.dataTask(with: request) { (_, _, error) in
+            // Add HTTP Headers
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let task = self.session.dataTask(with: request) {(_, _, error) in
                 if let error = error {
                     print("[Swifty Webhook] - \(error)")
                     return
                 }
             }
             task.resume()
+            RunLoop.current.run()
             
         } catch {
             print("[Swifty Webhook] - \(error)")
